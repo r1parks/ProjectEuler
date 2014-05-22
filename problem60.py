ddfea351d61526f3dfcd0579932b2aa186aa825c
Robert Parks <r1parks.ucsd@gmail.com>
2014-05-22 17:59:57 -0400

    switched to the real file

--- problem60.py
+++ problem60.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python
+
 import primes
 
+def split_prime(p):
+    """ attempts to split the prime into two primes """
+    str_p = "{}".format(p)
+    split_primes = []
+    for i in range(1, len(str_p)):
+        p1, p2 = long(str_p[:i]), long(str_p[i:])
+        if primes.is_prime(long(p1) and primes.is_prime(long(p2))):
+            split_primes.append((p1, p2))
+    return set(split_primes)
+
 if __name__ == '__main__':
-    pass
+    prime_sets = []
+    current_prime = 10
+    while True:
+        current_prime = primes.find_next_prime(current_prime + 1)
+        print "next prime: {}".format(current_prime)
