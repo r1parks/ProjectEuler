f5b7422258b4ff900c38c7505d46a0f6dc474ac8
Robert Parks <r1parks.ucsd@gmail.com>
2014-05-23 10:52:52 -0400

    finished problem60

--- primes.py
+++ primes.py
@@ -1,43 +1,53 @@
 import random
 
-def miller_rabin(n, k):
-    assert(n > 3)
-    assert(k > 0)
-    assert(n > k+4)
-    if pow(2, n-1, n) != 1:
+def _try_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
         return False
-    d = n-1
-    s = 0
-    while d % 2 == 0:
-        d /= 2
-        s += 1
-    bases = set()
-    while len(bases) < k:
-        new_base = random.randrange(2, n-2)
-        bases.add(new_base)
-    for a in bases:
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(s-1):
-            x = pow(x, 2, n)
-            if x == 1:
-                return False
-            if x == n - 1:
-                break
+    for i in range(s):
+        if pow(a, 2**i * d, n) == n-1:
+            return False
+    return True # n  is definitely composite
+ 
+def miller_rabin(n, _precision_for_huge_n=16):
+    if n in _known_primes or n in (0, 1):
+        return True
+    if any((n % p) == 0 for p in _known_primes):
         return False
-    return True
-
-small_primes = set()
-small_primes.update([2,3,5,7,11,13,17,19,23])
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+    if n < 1373653: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+    if n < 25326001: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+    if n < 118670087467: 
+        if n == 3215031751: 
+            return False
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+    if n < 2152302898747: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+    if n < 3474749660383: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+    if n < 341550071728321: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+    # otherwise
+    return not any(_try_composite(a, d, n, s) 
+                   for a in _known_primes[:_precision_for_huge_n])
+ 
+_known_primes = [2, 3]
+_known_primes += [x for x in range(5, 1000, 2) if miller_rabin(x)]
 
 def is_prime(n):
-    return (n in small_primes) or miller_rabin(n, 10)
+    if n < 2:
+        return False
+    else:
+        return miller_rabin(n)
 
 def find_next_prime(n):
-    n += 6 - ((n - 1) % 6)
-    while is_prime(n) != True:
-        n += 6
+    n += 1 if n % 2 == 0 else 0
+    while is_prime(n) == False:
+        n += 2
     return n
 
 if __name__ == '__main__':
--- problem60.py
+++ problem60.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python
 
 import primes
+import sys
 
 def split_prime(p):
-    """ attempts to split the prime into two primes """
     str_p = "{}".format(p)
-    split_primes = []
     for i in range(1, len(str_p)):
         p1, p2 = long(str_p[:i]), long(str_p[i:])
-        if primes.is_prime(long(p1) and primes.is_prime(long(p2))):
-            split_primes.append((p1, p2))
-    return set(split_primes)
+        if primes.is_prime(long(p1)) and primes.is_prime(long(p2)) and primes.is_prime(long("{}{}".format(p2, p1))):
+            return [p1, p2]
+
+def try_prime_pair(p1, p2):
+    return primes.is_prime(long("{}{}".format(p1,p2))) and primes.is_prime(long("{}{}".format(p2,p1)))
+
+def try_prime_test(possible_set):
+    ps = list(possible_set)
+    for i in range(len(ps)):
+        for j in range(i+1, len(ps)):
+            if not try_prime_pair(ps[i], ps[j]):
+                return False
+    return True
+
+def add_new_primes(new, old):
+    if frozenset(new) in old:
+        return
+    new_sets = [frozenset(new)]
+    for prime_set in old:
+        possible_set = prime_set.union(new)
+        if possible_set not in old and possible_set not in new_sets and try_prime_test(possible_set):
+            new_sets.append(possible_set)
+            if len(possible_set) == 5:
+                print possible_set
+                sys.exit(0)
+    old.update(new_sets)
 
 if __name__ == '__main__':
-    prime_sets = []
+    prime_sets = set()
     current_prime = 10
     while True:
         current_prime = primes.find_next_prime(current_prime + 1)
-        print "next prime: {}".format(current_prime)
+        new_primes = split_prime(current_prime)
+        if new_primes:
+            add_new_primes(new_primes, prime_sets)
