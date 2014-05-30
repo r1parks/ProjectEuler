988ffc486b073d15821e02913e9ec9fa97dbff73
Robert Parks <r1parks.ucsd@gmail.com>
2014-05-30 10:42:23 -0400

    refactoring

--- pe_tools.py
+++ pe_tools.py
@@ -11,7 +11,31 @@ def cf_to_rational(cf):
         c = Fraction(a) + Fraction(c.denominator, c.numerator)
     return c
 
+def repeating_cf(cf):
+    """
+    takes a list of the form [a0, a1, a2, a3, ...]
+    and interprets as a continued fraction of the form
+    [a0; (a1, a2, a3, ...)] where (a1, a2, a3, ...) repeats
+    and returns a generator which repeats that sequence
+    indefinitely.
+    """
+    assert len(cf) > 1
+    yield cf[0]
+    cf = cf[1:]
+    i = 0
+    while True:
+        yield cf[i]
+        i = (i + 1) % len(cf)
+
 def continued_fraction_sqrt(n):
+    """
+    returns a list [a0, a1, a2, a3,...]
+    Which can be intepreted as a continued fraction of the form
+    [a0; (a1, a2, a3, ...)]
+    where (a1, a2, a3, ...) repeats indefinitely.
+    Use repeating_cf to convert to an infinite generator
+    of continued fraction terms.
+    """
     a = a0 = approx_int_sqrt(n)
     m = 0
     d = 1
--- problem66.py
+++ problem66.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 
-import pe_tools
-
-def repeating_cf(cf):
-    assert len(cf) > 1
-    yield cf[0]
-    cf = cf[1:]
-    i = 0
-    while True:
-        yield cf[i]
-        i = (i + 1) % len(cf)
+from pe_tools import repeating_cf, continued_fraction_sqrt, cf_to_rational, is_square
 
 def test_pells_equation(x, d, y):
     return x*x - d*y*y == 1
@@ -17,9 +8,9 @@ def test_pells_equation(x, d, y):
 def solve_pells_for_x(d):
     i = 1
     while True:
-        cf = repeating_cf(pe_tools.continued_fraction_sqrt(d))
+        cf = repeating_cf(continued_fraction_sqrt(d))
         finite_cf = [cf.next() for _ in range(i)]
-        rational = pe_tools.cf_to_rational(finite_cf)
+        rational = cf_to_rational(finite_cf)
         if test_pells_equation(rational.numerator, d, rational.denominator):
             return rational.numerator
         i += 1
@@ -28,7 +19,7 @@ if __name__ == '__main__':
     bestD = 0
     bestX = 0
     for d in range(2, 1000):
-        if pe_tools.is_square(d):
+        if is_square(d):
             continue
         x = solve_pells_for_x(d)
         if x > bestX:
