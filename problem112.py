a1fc51d124d48c70e3d32f49c8b12f07ed548d8c
Robert Parks <r1parks.ucsd@gmail.com>
2014-06-16 00:08:18 -0400

    problem 112

---
+++ problem112.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from pe_tools import memoized
+
+def isIncreasing(n):
+    n = str(n)
+    for i in range(len(n)-1):
+        if n[i] > n[i+1]:
+            return False
+    return True
+
+def isDecreasing(n):
+    n = str(n)
+    for i in range(len(n)-1):
+        if n[i] < n[i+1]:
+            return False
+    return True
+
+def isBouncy(n):
+    return not (isIncreasing(n) or isDecreasing(n))
+
+@memoized
+def bouncyBelow(n):
+    if n < 100:
+        return 0
+    if n == 1000:
+        return 525 #given in the problem
+    return bouncyBelow(n-1) + int(isBouncy(n))
+
+def bouncyRatio(n):
+    return float(bouncyBelow(n)) / float(n)
+
+if __name__ == '__main__':
+    from itertools import count
+    for i in count(1):
+        if bouncyRatio(i) >= 0.99:
+            print "{}: {}".format(1000, bouncyBelow(1000))
+            print "{}".format(i)
+            break
